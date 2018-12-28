da385434bac62e442ce34586f8bace6894576428
Nano <hikarishido1997@gmail.com>
2018-12-28 18:38:57 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="w-screen h-screen overflow-hidden">
+    <div 
+      class="w-full h-full flex items-center justify-center"
+      style="background: rgba(0, 0, 0, .3);" 
+    >
+      hihi
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Cabin', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background: url(/background.png);
+    background-size: cover !important;
+    background-position: center !important; 
+  }
 </style>
