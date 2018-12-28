a9a636daa6ebbe165e7e82e88b109dcf4d29304a
Nano <hikarishido1997@gmail.com>
2018-12-28 21:20:47 +0700

    UI completed

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
   <div id="app" class="w-screen h-screen overflow-hidden">
-    <div 
-      class="w-full h-full flex items-center justify-center"
-      style="background: rgba(0, 0, 0, .3);" 
-    >
-      hihi
+    <div class="w-full h-full flex items-center justify-center">
+      <div class="self-end mb-32 w-3/5">
+        <div class="text-center">
+          <canvas ref="canvas" class="w-full h-32 border"></canvas>
+          <div class="w-full mb-1" style="height: 3px; background: rgba(255, 255, 255, .3);">
+            <div class="bg-white h-full" style="width: 10%;"></div>
+          </div>
+        </div>
+        <div class="flex">
+          <div class="mr-4">
+            <img
+              width="130"
+              height="130"
+              class="rounded-sm"
+              src="/thumbnails/meant for you - OMFG.jpg"
+            />
+          </div>
+          <div class="self-center mb-1 text-left">
+            <h1 class="text-white tracking-wide font-bold uppercase mb-1">Meant For You</h1>
+            <h3 class="text-grey-lighter tracking-wide font-hairline uppercase">OMFG</h3>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-  }
+  export default {}
 </script>
 
 <style>
@@ -19,8 +36,12 @@
     font-family: 'Cabin', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    background: url(/background.png);
+    background: url(/backgrounds/background2.jpg);
     background-size: cover !important;
-    background-position: center !important; 
+    background-position: center !important;
+  }
+
+  #app > div {
+    background: rgba(0, 0, 0, .3);
   }
 </style>
