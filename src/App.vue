da576a4c57bb0e9e2ff5f64a7718a714d83a0cbb
Nano <hikarishido1997@gmail.com>
2018-12-29 15:50:46 +0700

    prefetch

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,44 @@
 <template>
-  <div id="app" class="w-screen h-screen overflow-hidden">
-    <div class="w-full h-full flex items-center justify-center">
+  <div
+    id="app"
+    class="w-screen h-screen overflow-hidden relative"
+    :style="{ background: `url(/background/${background})` }"
+  >
+    <transition
+      name="custom-classes-transition"
+      enter-active-class="animated bounceInLeft"
+      leave-active-class="animated bounceOutRight"
+    >
+      <div
+        v-if="upNext"
+        id="upNext"
+        class="w-full h-full absolute pin-t pin-l"
+        :style="{ background: `url(/background/${background})` }"
+      >
+        <div
+          class="w-full h-full flex flex-col items-center justify-center"
+          style="background: rgba(0, 0, 0, .3);"
+        >
+          <h1
+            class="text-black bg-white px-4 py-3 uppercase rounded shadow-md text-4xl text-center font-bold tracking-wide mt-8"
+            style="opacity: .9; min-width: 25%; max-width: 65%;"
+          >
+            <i class="fa fa-forward"></i>
+            {{ upNextSong.name }}
+          </h1>
+          <span
+            class="text-white bg-black px-4 py-3 tracking-wide uppercase shadow-md text-center rounded"
+            style="opacity: .9; min-width: 15%; max-width: 50%;"
+          >
+            {{ upNextSong.artist }}
+          </span>
+        </div>
+      </div>
+    </transition>
+    <div
+      class="w-full h-full flex items-center justify-center"
+      style="background: rgba(0, 0, 0, .3);"
+    >
       <div class="self-end mb-32 w-3/5">
         <div class="text-center">
           <canvas ref="canvas" class="w-full h-32"></canvas>
@@ -18,8 +56,8 @@
             />
           </div>
           <div class="self-center mb-1 text-left">
-            <h1 class="text-white tracking-wide font-bold uppercase mb-1">{{ songInfo.name }}</h1>
-            <h3 class="text-grey-lighter tracking-wide font-hairline uppercase">{{ songInfo.artist }}</h3>
+            <h1 class="text-white tracking-wide font-bold uppercase mb-1">{{ getSongInfo.name }}</h1>
+            <h3 class="text-grey-lighter tracking-wide font-hairline uppercase">{{ getSongInfo.artist }}</h3>
           </div>
         </div>
       </div>
@@ -35,21 +73,32 @@
       return {
         songs: [],
         thumbnails: [],
-        index: 0,
+        background: '',
+        index: 4,
         audio: new Audio,
-        progress: '0%'
+        progress: '0%',
+        upNext: false,
+        upNextSong: {}
       }
     },
     methods: {
       async fetchData() {
-        const dataPaths = ['/songs', '/thumbnails']
-        const promises = await Promise.all(dataPaths.map(path => axios.get(path)))
+        const dataPaths = ['/songs', '/thumbnails', '/background']
+        const promises  = await Promise.all(dataPaths.map(path => axios.get(path)))
 
-        this.songs = [...promises[0].data]
+        this.songs      = [...promises[0].data]
         this.thumbnails = [...promises[1].data]
+        this.background = promises[2].data
       },
 
-      init() {
+      songInfo(song) {
+        const re = /\.(mp3|aac|ogg|m4a|wma|flac|wav)$/i
+        const [ name, artist ] = song.replace(re, '').split('-')
+
+        return { name, artist }
+      },
+
+      initPlayer() {
         const canvas = this.$refs.canvas
         const ctx = canvas.getContext("2d")
         const { width, height } = canvas
@@ -89,6 +138,13 @@
       timeUpdate() {
         const newTime = this.audio.currentTime * (100 / this.audio.duration)
         this.progress = newTime + '%'
+
+        if(Math.floor(this.audio.duration - this.audio.currentTime) === 5) {
+          if(this.songs[this.index + 1]) {
+            this.upNext = true
+            this.upNextSong = this.songInfo(this.songs[this.index + 1])
+          }
+        }
       },
 
       nextSong() {
@@ -96,17 +152,29 @@
           this.index++
           this.audio.src = `/songs/${this.songs[this.index]}`
           this.audio.play()
+
+          setTimeout(() => {
+            this.upNext = false
+            this.preFetch()
+          }, 500)
         }
-        else console.log('end')
+        else console.log('tắt nghỉ :))')
+      },
+
+      preFetch() {
+        const nextIndex = this.index + 1
+        const nextDataPaths = [
+          `/thumbnails/${this.thumbnails[nextIndex]}`,
+          `/songs/${this.songs[nextIndex]}`,
+        ]
+
+        Promise.all(nextDataPaths.map(path => axios.get(path)))
       }
     },
     computed: {
-      songInfo() {
-        if(this.songs[this.index]) {
-          const re = /\.(mp3|aac|ogg|m4a|wma|flac|wav)$/i
-          const [ name, artist ] = this.songs[this.index].replace(re, '').split('-')
-          return { name, artist }
-        }
+      getSongInfo() {
+        if(this.songs[this.index])
+          return this.songInfo(this.songs[this.index])
 
         return {
           name: 'Dunno',
@@ -117,9 +185,11 @@
     async created() {
       await this.fetchData()
 
-      this.init()
+      this.initPlayer()
       this.audio.addEventListener('timeupdate', this.timeUpdate)
       this.audio.addEventListener('ended', this.nextSong)
+
+      this.preFetch()
     }
   }
 </script>
@@ -129,12 +199,11 @@
     font-family: 'Cabin', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    background: url(/backgrounds/background2.jpg);
-    background-size: cover !important;
-    background-position: center !important;
   }
 
-  #app > div {
-    background: rgba(0, 0, 0, .3);
+  #app,
+  #upNext {
+    background-size: cover !important;
+    background-position: center !important;
   }
 </style>
